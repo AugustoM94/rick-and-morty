43e6862f29d9c73edf26887d73688fc29f56e858
AugustoM94 <email@github.com>
2023-11-09 15:55:34 +0100

    added HeaderApp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-
+  <div class="container">
+   <HeaderApp />
   </div>
 </template>
 
 <script>
+import HeaderApp from "./components/HeaderApp.vue";
   export default {
-    
+    components: {
+      HeaderApp,
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div>
+            <img src="/public/vue-dc-comics-1/img/dc-logo.png" alt="logo-dc">
+
+        </div>
+         <div>
+            <ul>
+                <li><a href="#"></a></li>
+            </ul>
+         </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: HeaderApp,
+        data(){
+            return {
+                 links: [
+                      {
+                        text: "Characters"
+                      },
+                      {
+                        text: "Comics"
+                      },
+                      {
+                        text: "Movies"
+                      },
+                      {
+                        text: "TV"
+                      },
+                      {
+                        text: "Collectibles"
+                      },
+                      {
+                        text: "Videos"
+                      },
+                      {
+                        text: "Fans"
+                      },
+                      {
+                        text: "News"
+                      },
+                      {
+                        text: "Shop"
+                      },
+                 ]
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
